848bff1ca7f0a872b4fd91d3d8796124c2e9945d
jacds <bclarinetman@gmail.com>
2016-04-21 03:12:07 +0100

    Data is separated by the percentages received from the GUI. Also, the means for each class are calculated. Some of the work left to implement is specified in the code, although some might still be missing (probably).

--- main.m
+++ main.m
@@ -1,2 +1,35 @@
-function main()
+function main(handles)
+
+%Calculates amount of train and test data
+[len,~] = size(handles.data);
+train_amount = int32((str2num(get(handles.edit_train,'String'))/100)*len);
+test_amount = len-train_amount;
+
+%Separates train and test values in input data
+train_data = handles.selected_columns(1:train_amount,:);
+test_data = handles.selected_columns(train_amount+1:len,:);
+
+%Separates train and test values in target data
+train_target = handles.target(1:train_amount,:);
+test_target = handles.target(train_amount+1:len,:);
+
+%TODO: Check Scaling
+
+%TODO: Check PCA/LDA
+train_data = train_data';
+
+[~, n_dimensions] = size(handles.selected_columns);
+
+%Calculate mean for each class
+average = zeros(n_dimensions,2);
+for i=1 : n_dimensions
+    average(i,1) = mean(train_data(i, train_target==0));
+    average(i,2) = mean(train_data(i, train_target==1));
+end
+
+%TODO Euclidean Distance to Classify
+
+%TODO Compare?
+
+
 end
