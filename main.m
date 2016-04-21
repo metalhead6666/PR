10faed6666336caf5ec75a2f70691e9ead3fbd73
jacds <bclarinetman@gmail.com>
2016-04-21 19:09:01 +0100

    Inserted the euclidean distance part. Wrote the code for the classifier, as well.

--- main.m
+++ main.m
@@ -1,4 +1,6 @@
-function main(handles)
+function [target, predicted] = main(handles)
+
+clc
 
 %Calculates amount of train and test data
 [len,~] = size(handles.data);
@@ -16,18 +18,40 @@ test_target = handles.target(train_amount+1:len,:);
 %TODO: Check Scaling
 
 %TODO: Check PCA/LDA
+
+
+
+%Calculate mean for each class
 train_data = train_data';
+size(train_data);
 
 [~, n_dimensions] = size(handles.selected_columns);
 
-%Calculate mean for each class
 average = zeros(n_dimensions,2);
 for i=1 : n_dimensions
     average(i,1) = mean(train_data(i, train_target==0));
     average(i,2) = mean(train_data(i, train_target==1));
 end
 
+
 %TODO Euclidean Distance to Classify
+[len,n_dimensions] = size(test_data);
+
+results = zeros(1,len);
+for i=1 : len
+    %disp(test_data(i,:));
+    %disp(average(:,1));
+    results(i) = euclidean(test_data(i,:),average);
+end
+%disp('Euclidean Done');
+test_target = test_target';
+
+%size(test_target)
+%size(results)
+%plotconfusion(test_target,results);
+
+target = test_target;
+predicted = results;
 
 %TODO Compare?
 
